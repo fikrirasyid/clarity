129740c7ba0b579d8809b6dffd98b62337416e21
Fikri Rasyid <fikrirasyid@gmail.com>
2014-09-05 09:16:26 +0700

    Wrapping template with .wrap and .content-wrap

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
+		<div class="site-info wrap">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'clarity' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'clarity' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( __( 'Theme: %1$s by %2$s.', 'clarity' ), 'Clarity', '<a href="http://fikrirasyid.com" rel="designer">Fikri Rasyid</a>' ); ?>
